dd7f5ef8aa4309a17e6e63cd17123bdbf5741fc8
suleymannmeral <mrlslymn02@gmail.com>
2025-03-03 16:45:54 +0300

    hafta4odev1

---
+++ hafta4Odevleri/acunMedyaWeek4(odev1)/acunMedyaWeek4/Abstract/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acunMedyaWeek4.Abstract
+{
+    public abstract class Employee
+    {
+        public abstract double SalaryInfo();
+        public void StartStaff(string name)
+        {
+            Console.WriteLine($"{name} is starting to work ");
+        }
+    }
+}
