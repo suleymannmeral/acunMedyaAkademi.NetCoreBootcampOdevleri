d15f7ab800e86a5f2f91e532a5b1b2e58427f462
suleymannmeral <mrlslymn02@gmail.com>
2025-03-10 20:49:11 +0300

    odev1 uploaded

---
+++ hafta5Odevleri/odev1/hafta5Odev1/hafta5Odev1/AgeControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hafta5Odev1
+{
+    class AgeControl
+    {
+        public string CheckAge(int age)
+        {
+            if (age > 0 && age < 18)
+                return "Küçüksünüz";
+
+            else if (age >= 18 && age < 35)
+                return "Gençsiniz";
+
+            else if (age >= 35 && age < 55)
+
+                return "Yetişkinsiniz";
+            else if (age >= 55 && age < 75)
+                return "Yaşlısınız";
+
+            else if (age >= 75 && age < 99)
+                return "Çok Yaşlısınız";
+
+            else if (age < 0 && age > 99)
+                return "Ya hiç doğmadınız ya da çoktan öldünüz";
+
+            else
+                return null;
+
+
+        }
+    }
+}
---
+++ hafta5Odevleri/odev1/hafta5Odev1/hafta5Odev1/Program.cs
@@ -0,0 +1,9 @@
+﻿using hafta5Odev1;
+
+AgeControl age = new AgeControl();
+
+Console.WriteLine(age.CheckAge(22));
+Console.WriteLine(age.CheckAge(100));
+Console.WriteLine(age.CheckAge(10));
+Console.WriteLine(age.CheckAge(60));
+Console.WriteLine(age.CheckAge(78));
