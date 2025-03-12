e7a72e0e0bdba6f5fc68792eb862bc8ad76417df
suleymannmeral <mrlslymn02@gmail.com>
2025-03-12 12:36:44 +0300

    Odev 4

---
+++ hafta5Odevleri/odev4/hafta5Odev4/hafta5Odev4/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hafta5Odev4
+{
+    class Employee
+    {
+        public Employee(int maas, string? ad, string? soyad, string? meslek, string? departman)
+        {
+            Maas = maas;
+            Ad = ad;
+            Soyad = soyad;
+            Meslek = meslek;
+            Departman = departman;
+        }
+
+        public int Maas { get; set; }
+        public string? Ad { get; set; }
+        public string? Soyad  { get; set; }
+        public string? Meslek  { get; set; }
+        public string? Departman  { get; set; }
+    }
+}
---
+++ hafta5Odevleri/odev4/hafta5Odev4/hafta5Odev4/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using hafta5Odev4;
+
+Employee[] calisanlar = new Employee[]
+      {
+            new Employee(40000,"Süleyman","Meral","Stajyer","IT"),
+            new Employee(22000,"Beyza","Meral","Programcı","IT"),
+            new Employee(100000,"Esma","Yağbakar","Genel Müdür","Eğitim"),
+            new Employee(45000,"Ecrin","Kabil","Müdür","Yapı"),
+            new Employee(90000,"Oğuzhan","Meral","Genel Müdür","Teknik"),
+        
+      };
+
+double toplamMaas = 0;
+
+for(int i=0; i<calisanlar.Length;i++)
+{
+    Console.WriteLine(calisanlar[i].Ad + " " + calisanlar[i].Soyad + " " + calisanlar[i].Meslek + " " + calisanlar[i].Departman);
+    toplamMaas += calisanlar[i].Maas;
+
+}
+Console.WriteLine("Toplam Maas:" + toplamMaas);
