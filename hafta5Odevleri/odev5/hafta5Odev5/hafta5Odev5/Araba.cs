b1b41acf6eaba6e0d34cd67724cde96f8c72046a
suleymannmeral <mrlslymn02@gmail.com>
2025-03-12 12:46:35 +0300

    odev 5

---
+++ hafta5Odevleri/odev5/hafta5Odev5/hafta5Odev5/Araba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hafta5Odev5
+{
+    class Araba
+    {
+        public Araba(string? marka, int benzin)
+        {
+            Marka = marka;
+            Benzin = benzin;
+        }
+
+        public string? Marka { get; set; }
+        public int Benzin { get; set; }
+    }
+}
---
+++ hafta5Odevleri/odev5/hafta5Odev5/hafta5Odev5/Program.cs
@@ -0,0 +1,21 @@
+﻿using hafta5Odev5;
+
+
+Araba[] arabalar = new Araba[]
+        {
+            new Araba("Ford",150), 
+            new Araba("BMW",200), 
+            new Araba("Audi",300), 
+            new Araba("Porshce",2500), 
+            new Araba("Porshce",2500), 
+
+        };
+
+int toplamBenzin = 0;
+
+foreach(var item in arabalar)
+{
+    toplamBenzin += item.Benzin;
+}
+
+Console.WriteLine(toplamBenzin);
