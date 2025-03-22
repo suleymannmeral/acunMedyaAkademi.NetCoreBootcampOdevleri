ea0c8ce6b94df65fd8d859a46034fb072a9770a5
suleymannmeral <mrlslymn02@gmail.com>
2025-03-22 15:55:35 +0300

    sayiTahminOyunu

---
+++ sayiTahminOyunu/acunMedyaAkademiSayiTahminOyunu/acunMedyaAkademiSayiTahminOyunu/Program.cs
@@ -0,0 +1,34 @@
+﻿Random random = new Random();
+int rastgeleSayi = random.Next(1, 101);
+int tahmin;
+int tahminSayisi = 0;
+
+Console.WriteLine("1 ile 100 arasında bir sayı tahmin edin!");
+
+do
+{
+    Console.Write("Tahmininizi girin: ");
+    string input = Console.ReadLine();
+
+    if (!int.TryParse(input, out tahmin))
+    {
+        Console.WriteLine("Lütfen geçerli bir sayı girin!");
+        continue;
+    }
+
+    tahminSayisi++;
+
+    if (tahmin < rastgeleSayi)
+    {
+        Console.WriteLine("Daha büyük bir sayı girin!");
+    }
+    else if (tahmin > rastgeleSayi)
+    {
+        Console.WriteLine("Daha küçük bir sayı girin!");
+    }
+    else
+    {
+        Console.WriteLine($"Tebrikler {tahminSayisi} tahminde doğru sayıyı buldunuz");
+        break;
+    }
+} while (true);
