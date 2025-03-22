edee3b0ad2c65887e53fe54772647c163e12be19
suleymannmeral <mrlslymn02@gmail.com>
2025-03-22 16:01:08 +0300

    dizivedonguodevi

---
+++ diziVeDonguOdevi/acunMedyaAkademiDiziVeDongu/acunMedyaAkademiDiziVeDongu/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] sayilar = new int[5];
+int toplam = 0;
+
+Console.WriteLine("Lütfen 5 sayı girin:");
+
+for (int i = 0; i < sayilar.Length; i++)
+{
+    Console.Write($"Sayı {i + 1}: ");
+    while (!int.TryParse(Console.ReadLine(), out sayilar[i])) 
+    {
+        Console.Write("Geçerli bir sayı girin: ");
+    }
+}
+
+foreach (int sayi in sayilar)
+{
+    toplam += sayi;
+}
+
+double ortalama = (double)toplam / sayilar.Length;
+
+
+Console.WriteLine($"Toplam: {toplam}");
+Console.WriteLine($"Ortalama: {ortalama:F2}");
