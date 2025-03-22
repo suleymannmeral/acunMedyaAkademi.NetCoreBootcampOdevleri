b7f80f14f168ed2524b876aca2c86c2a12d62558
suleymannmeral <mrlslymn02@gmail.com>
2025-03-22 15:48:56 +0300

    odev2

---
+++ hafta6Odevleri/odev2/AcunMedyaCompositionAndOverloadingOdevi-2/AcunMedyaCompositionAndOverloadingOdevi-2/MathCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcunMedyaCompositionAndOverloadingOdevi_2
+{
+    class MathCalculator
+    {
+        public int Sum(int a,int b)
+        {
+            return a + b;
+        }
+
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public int Sum(int a , int b , int c)
+        {
+            return a + b + c;
+        }
+
+        public int Carp(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Carp(double a, double b)
+        {
+            return a * b;
+        }
+
+        public int Carp(int a, int b, int c)
+        {
+            return a * b *c;
+        }
+    }
+}
---
+++ hafta6Odevleri/odev2/AcunMedyaCompositionAndOverloadingOdevi-2/AcunMedyaCompositionAndOverloadingOdevi-2/Program.cs
@@ -0,0 +1,7 @@
+﻿using AcunMedyaCompositionAndOverloadingOdevi_2;
+
+MathCalculator math = new MathCalculator();
+
+Console.WriteLine(math.Carp(2, 3, 7));
+Console.WriteLine(math.Carp(2.5, 3.7));
+Console.WriteLine(math.Carp(1, 5));
