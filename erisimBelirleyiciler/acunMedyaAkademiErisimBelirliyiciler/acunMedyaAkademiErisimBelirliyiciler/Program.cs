793ae5e48ee43e4616266c02ddfa8a368cdada2e
suleymannmeral <mrlslymn02@gmail.com>
2025-03-23 09:09:58 +0300

    erisim belirleyiciler

---
+++ erisimBelirleyiciler/acunMedyaAkademiErisimBelirliyiciler/acunMedyaAkademiErisimBelirliyiciler/Program.cs
@@ -0,0 +1,9 @@
+﻿using acunMedyaAkademiErisimBelirliyiciler;
+
+User user = new User();
+
+
+user.SetAdSoyad("Süleyman Meral");
+user.SetYas(22);
+user.Mail = "suleyman@abc.com";
+user.BilgileriGoster();
---
+++ erisimBelirleyiciler/acunMedyaAkademiErisimBelirliyiciler/acunMedyaAkademiErisimBelirliyiciler/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acunMedyaAkademiErisimBelirliyiciler
+{
+    class User
+    {
+        private string? adSoyad;
+        private int yas;
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void SetAdSoyad(string value)
+        {
+            adSoyad = value;
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+
+        public void SetYas(int value)
+        {
+            if (value >= 0) 
+            {
+                yas = value;
+            }
+            else
+            {
+                Console.WriteLine("Yaş negatif olamaz!");
+            }
+        }
+        public string? Mail { get; set; }
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Ad Soyad: {adSoyad}");
+            Console.WriteLine($"Yaş: {yas}");
+            Console.WriteLine($"E-posta: {Mail}");
+        }
+    }
+}
