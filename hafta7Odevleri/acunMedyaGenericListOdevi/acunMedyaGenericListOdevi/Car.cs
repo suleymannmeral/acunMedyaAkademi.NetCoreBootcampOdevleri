0e2dc5d38dbc9b1d6ff41b1b097db9648af2e6fc
suleymannmeral <mrlslymn02@gmail.com>
2025-03-24 09:25:14 +0300

    Hafta7Odevleri

---
+++ hafta7Odevleri/acunMedyaGenericListOdevi/acunMedyaGenericListOdevi/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acunMedyaGenericListOdevi
+{
+    class Car
+    {
+        public string? Marka { get; set; }
+        public string? Model { get; set; }
+        public double? BenzinHarcamasi { get; set; }
+        public double? ToplamMesafe { get; set; }
+
+        public double YakitTuketimi(double BenzinHarcamasi,double ToplamMesafe)
+        {
+            return (ToplamMesafe / 100) * BenzinHarcamasi;
+        }
+
+       
+    }
+}
---
+++ hafta7Odevleri/acunMedyaGenericListOdevi/acunMedyaGenericListOdevi/Program.cs
@@ -0,0 +1,27 @@
+﻿using acunMedyaGenericListOdevi;
+Car bmw = new Car();
+bmw.Marka = "BMW";
+bmw.Model = "X5";
+bmw.BenzinHarcamasi = 9;
+bmw.ToplamMesafe = 100;
+Car mercedes = new Car();
+mercedes.Marka = "Mercedes";
+mercedes.Model = "C200";
+mercedes.BenzinHarcamasi = 8.2;
+mercedes.ToplamMesafe = 100;
+Car toyota = new Car();
+toyota.Marka = "Toyota";
+toyota.Model = "Corolla";
+toyota.BenzinHarcamasi = 6.5;
+toyota.ToplamMesafe = 100;
+List<Car> cars = new List<Car>();
+
+cars.Add(bmw);
+cars.Add(mercedes);
+cars.Add(toyota);
+
+foreach(var item in cars)
+{
+  
+    Console.WriteLine("Marka: " + item.Marka + ", Model: " + item.Model + ", 100KM'de Yaktığı Yakıt: " + item.BenzinHarcamasi + " Toplam Yakıt Tüketimi: " + item.YakitTuketimi((double)item.BenzinHarcamasi, (double)item.ToplamMesafe)); ;
+}
