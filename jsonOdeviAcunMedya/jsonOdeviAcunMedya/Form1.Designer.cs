2f113b9cae8a47b0a20f48b92c7d01a52dc1e704
suleymannmeral <mrlslymn02@gmail.com>
2025-04-15 13:52:36 +0300

    Json Odevi

---
+++ jsonOdeviAcunMedya/jsonOdeviAcunMedya/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace jsonOdeviAcunMedya
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtProductName = new TextBox();
+            txtPrice = new TextBox();
+            btnJsonCreate = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(191, 142);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Ürün Adı:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(202, 213);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Fiyat:";
+            // 
+            // txtProductName
+            // 
+            txtProductName.Location = new Point(281, 139);
+            txtProductName.Name = "txtProductName";
+            txtProductName.Size = new Size(255, 27);
+            txtProductName.TabIndex = 2;
+            // 
+            // txtPrice
+            // 
+            txtPrice.Location = new Point(281, 210);
+            txtPrice.Name = "txtPrice";
+            txtPrice.Size = new Size(255, 27);
+            txtPrice.TabIndex = 3;
+            // 
+            // btnJsonCreate
+            // 
+            btnJsonCreate.Location = new Point(229, 377);
+            btnJsonCreate.Name = "btnJsonCreate";
+            btnJsonCreate.Size = new Size(148, 29);
+            btnJsonCreate.TabIndex = 4;
+            btnJsonCreate.Text = "Json'a Çevir";
+            btnJsonCreate.UseVisualStyleBackColor = true;
+            btnJsonCreate.Click += btnJsonCreate_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(407, 377);
+            button1.Name = "button1";
+            button1.Size = new Size(155, 30);
+            button1.TabIndex = 5;
+            button1.Text = "Json Veri Oku";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(812, 551);
+            Controls.Add(button1);
+            Controls.Add(btnJsonCreate);
+            Controls.Add(txtPrice);
+            Controls.Add(txtProductName);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtProductName;
+        private TextBox txtPrice;
+        private Button btnJsonCreate;
+        private Button button1;
+    }
+}
---
+++ jsonOdeviAcunMedya/jsonOdeviAcunMedya/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+
+namespace jsonOdeviAcunMedya
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJsonCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filePath = @"C:\Users\suley\source\repos\jsonOdeviAcunMedya\product.json";
+
+                List<Product> products = new List<Product>();
+                if (File.Exists(filePath))
+                {
+                    using (StreamReader sr = new StreamReader(filePath))
+                    using (JsonReader jr = new JsonTextReader(sr))
+                    {
+                        JsonSerializer jsonSerializer = new JsonSerializer();
+                        products = jsonSerializer.Deserialize<List<Product>>(jr) ?? new List<Product>();
+                    }
+                }
+
+
+                Product product = new Product
+                {
+                    Name = txtProductName.Text,
+                    Price = Convert.ToDecimal(txtPrice.Text)
+                };
+
+                products.Add(product);
+
+
+                JsonSerializer serializer = new JsonSerializer();
+                using (StreamWriter sw = new StreamWriter(filePath))
+                using (JsonWriter jw = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(jw, products);
+                    MessageBox.Show("Json serileştirme tamamlandı");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu : " + ex.Message.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filePath = @"C:\Users\suley\source\repos\jsonOdeviAcunMedya\product.json";
+
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show("Dosya bulunamadı.");
+                    return;
+                }
+
+                List<Product> products = new List<Product>();
+
+                using (StreamReader sr = new StreamReader(filePath))
+                using (JsonReader jr = new JsonTextReader(sr))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    products = serializer.Deserialize<List<Product>>(jr);
+                }
+
+                if (products != null && products.Count > 0) // liste boş degilse 
+                {
+                    string message = "";
+                    foreach (var product in products)
+                    {
+                        message += $"Ürün Adı: {product.Name}, Fiyat: {product.Price}\n";
+                    }
+
+                    MessageBox.Show(message, "Kayıtlı Ürünler");
+                }
+                else
+                {
+                    MessageBox.Show("Ürün listesi boş.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu : " + ex.Message.ToString());
+            }
+        }
+    }
+}
