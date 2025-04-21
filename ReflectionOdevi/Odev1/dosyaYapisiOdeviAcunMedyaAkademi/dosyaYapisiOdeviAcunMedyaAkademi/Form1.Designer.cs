5601666764cb551220086adf17c59283cf1d55ac
suleymannmeral <mrlslymn02@gmail.com>
2025-04-21 18:58:53 +0300

    ReflectionOdevi1

---
+++ ReflectionOdevi/Odev1/dosyaYapisiOdeviAcunMedyaAkademi/dosyaYapisiOdeviAcunMedyaAkademi/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace dosyaYapisiOdeviAcunMedyaAkademi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            btnSelectFile = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(282, 57);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(430, 284);
+            listBox1.TabIndex = 0;
+            // 
+            // btnSelectFile
+            // 
+            btnSelectFile.Location = new Point(418, 395);
+            btnSelectFile.Name = "btnSelectFile";
+            btnSelectFile.Size = new Size(152, 52);
+            btnSelectFile.TabIndex = 1;
+            btnSelectFile.Text = "Select File";
+            btnSelectFile.UseVisualStyleBackColor = true;
+            btnSelectFile.Click += btnSelectFile_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(989, 538);
+            Controls.Add(btnSelectFile);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button btnSelectFile;
+    }
+}
---
+++ ReflectionOdevi/Odev1/dosyaYapisiOdeviAcunMedyaAkademi/dosyaYapisiOdeviAcunMedyaAkademi/Form1.cs
@@ -0,0 +1,47 @@
+﻿namespace dosyaYapisiOdeviAcunMedyaAkademi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)  //kullanıcı ok butonuna bastıysa 
+            {
+                listBox1.Items.Clear();
+                string selectedPath = dialog.SelectedPath;  // seçilen klasörün pathi(yolu) selectedPath değişkenine atandı
+
+                
+                string[] folders = Directory.GetDirectories(selectedPath); // pathdeki klasörler folders adında string bir değere atandı
+
+                foreach (string folder in folders)  // dizinin elemanları foreach loop ile sırasıyla işleniyor. 
+                {
+                    string folderName = Path.GetFileName(folder);   
+                    listBox1.Items.Add("Folder->" + folderName);  // klasör ismi listboxa yazdırılıyor 
+
+               
+                    string[] csFiles = Directory.GetFiles(folder, "*.cs"); // klasördeki .cs uzantılı tüm dosyalar csFiles dizisine atanıyor * simgesi all anlamına gelir.
+
+                    foreach (string file in csFiles)
+                    {
+                        string[] lines = File.ReadAllLines(file); 
+                        foreach (string line in lines)
+                        {
+                            if (line.Contains("class "))
+                            {
+                                string className = line.Trim().Split(' ')[^1];
+                                listBox1.Items.Add(" Class-> " + className); // class isimleri listboxa ekleniyor
+                                break; 
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
